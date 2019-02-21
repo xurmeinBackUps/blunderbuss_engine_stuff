5c31db1999591bf470285bf4dc0a6f3546d7f287
Xurmein <wj.pavocrist@gmail.com>
2019-02-20 20:20:52 -0500

    cleaning up

--- src/app/protected/user-interface/user-nav/user-nav.module.ts
+++ src/app/protected/user-interface/user-nav/user-nav.module.ts
@@ -10,13 +10,25 @@ import {
 
 import { UserNavRoutingModule } from './user-nav-routing.module';
 import { NavComponent } from './nav/nav.component';
-import { ViewsModule } from './views/views.module';
+import { BonesModule } from './views/bones/bones.module';
+import { DiceRollerModule } from './views/dice-roller/dice-roller.module';
+import { FleshModule } from './views/flesh/flesh.module';
+import { GroveModule } from './views/grove/grove.module';
+import { PcBuilderModule } from './views/pc-builder/pc-builder.module';
+import { SheetMakerModule } from './views/sheet-maker/sheet-maker.module';
+import { SpellcardMakerModule } from './views/spellcard-maker/spellcard-maker.module';
 
 @NgModule({
   declarations: [NavComponent],
   imports: [
     CommonModule,
-    ViewsModule,
+    BonesModule,
+    DiceRollerModule,
+    FleshModule,
+    GroveModule,
+    PcBuilderModule,
+    SheetMakerModule,
+    SpellcardMakerModule,
     UserNavRoutingModule,
     MatButtonModule,
     MatSidenavModule,
